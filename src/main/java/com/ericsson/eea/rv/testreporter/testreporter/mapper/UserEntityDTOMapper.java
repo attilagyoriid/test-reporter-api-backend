e00455bf9403ee4a7def28a29cadd71e241baed1
Attila Gyori <attila.gyori@ericsson.com>
2019-04-05 09:46:46 +0200

    image mapping - converter

--- src/main/java/com/ericsson/eea/rv/testreporter/testreporter/mapper/UserEntityDTOMapper.java
+++ src/main/java/com/ericsson/eea/rv/testreporter/testreporter/mapper/UserEntityDTOMapper.java
@@ -3,12 +3,13 @@ package com.ericsson.eea.rv.testreporter.testreporter.mapper;
 import com.ericsson.eea.rv.testreporter.testreporter.domain.User;
 import com.ericsson.eea.rv.testreporter.testreporter.model.UserAccountDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserEntityDTOMapper {
     UserEntityDTOMapper INSTANCE = Mappers.getMapper(UserEntityDTOMapper.class);
-
+    @Mapping(target="image", source="userEntity.image")
     UserAccountDTO userEntityToUserAccountDTO(User userEntity);
     User userAccountDTOToUserEntity(UserAccountDTO userAccountDTO);
 }
